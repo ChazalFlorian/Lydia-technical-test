96cad2133935542c4339e78a30b4e5a7cbc7db07
ChazalFlorian <chazal.florian@gmail.com>
2020-08-19 01:02:12 +0200

    test: display result in logcat for testing purpose

--- app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
+++ app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
@@ -2,11 +2,28 @@ package fr.florian.lydia.technicaltest.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import fr.florian.lydia.technicaltest.R
+import fr.florian.lydia.technicaltest.data.models.User
+import fr.florian.lydia.technicaltest.ui.viewmodels.SplashViewModel
+import java.io.Console
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var userViewModel: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        userViewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
+
+        userViewModel.data.observe(this, Observer {
+            it.forEach { user ->
+                Log.d("SPLASH", user.email)
+            }
+        })
     }
 }
