782b98707bc82a4fa67bf1c09d58a3b52d1a9fab
ChazalFlorian <chazal.florian@gmail.com>
2020-08-18 22:06:23 +0200

    feat: add empty Splash Activity

--- app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
+++ app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
@@ -2,6 +2,7 @@ package fr.florian.lydia.technicaltest.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.florian.lydia.technicaltest.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
