a578b675ad77928026e3bd27455fb2e7e7f910ce
ChazalFlorian <chazal.florian@gmail.com>
2020-08-19 01:01:31 +0200

    add: BaseViewModel parent class & SplahViewModel, which inject UserRepository and call for 1 batch of 10 users

---
+++ app/src/main/java/fr/florian/lydia/technicaltest/ui/viewmodels/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package fr.florian.lydia.technicaltest.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import fr.florian.lydia.technicaltest.injection.components.ViewModelInjector
+import fr.florian.lydia.technicaltest.injection.modules.NetworkModule
+
+abstract class BaseViewModel: ViewModel() {
+
+}
