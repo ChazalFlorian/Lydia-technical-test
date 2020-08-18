14d0d657175800415cc13a4668b3f6a7d04a5eb0
ChazalFlorian <chazal.florian@gmail.com>
2020-08-19 00:58:42 +0200

    feat: add retrofit route and BASE_URL to fetch from randomuser API

---
+++ app/src/main/java/fr/florian/lydia/technicaltest/data/remote/routes/UserApi.kt
@@ -0,0 +1,17 @@
+package fr.florian.lydia.technicaltest.data.remote.routes
+
+import fr.florian.lydia.technicaltest.data.models.Result
+import fr.florian.lydia.technicaltest.data.models.User
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UserApi {
+
+    @GET(" ")
+    suspend fun getUsersBatch(
+        @Query("results") results: Int,
+        @Query("page") page: Int,
+        @Query("seed") seed: String = "lydia"
+    ): Result<User>
+
+}
