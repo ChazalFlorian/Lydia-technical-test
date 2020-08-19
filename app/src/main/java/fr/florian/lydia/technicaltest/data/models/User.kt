9d04b36a49e6f1808289ef2a04ebbc1c56c6081d
ChazalFlorian <chazal.florian@gmail.com>
2020-08-19 15:01:34 +0200

    update: make user parcelable for navigation

--- app/src/main/java/fr/florian/lydia/technicaltest/data/models/User.kt
+++ app/src/main/java/fr/florian/lydia/technicaltest/data/models/User.kt
@@ -1,7 +1,9 @@
 package fr.florian.lydia.technicaltest.data.models
 
-import androidx.annotation.Nullable
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class User(
     val gender: String,
     val name: Name,
@@ -15,20 +17,24 @@ data class User(
     val id: Id,
     val picture: Picture,
     val nat: String
-) {
+) : Parcelable {
+
+    @Parcelize
     data class Name(
         val title: String,
         val first: String,
         val last: String
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class Location(
         val street: String,
         val city: String,
         val state: String,
         val postcode: Int
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class Login(
         val username: String,
         val password: String,
@@ -36,18 +42,20 @@ data class User(
         val md5: String,
         val sha1: String,
         val sha256: String
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class Id(
         val name: String,
         val value: String?
-    )
+    ) : Parcelable
 
+    @Parcelize
     data class Picture(
         val large: String,
         val medium: String,
         val thumbnail: String
-    )
+    ) : Parcelable
 
 
 }
