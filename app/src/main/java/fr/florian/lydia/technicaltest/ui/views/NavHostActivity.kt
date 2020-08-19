5e97583a1d99a7e83f5d9976f08c1e6927adaaf8
ChazalFlorian <chazal.florian@gmail.com>
2020-08-19 15:02:40 +0200

    add: Nav Activity update: splash activity to fire the nav activity

---
+++ app/src/main/java/fr/florian/lydia/technicaltest/ui/views/NavHostActivity.kt
@@ -0,0 +1,12 @@
+package fr.florian.lydia.technicaltest.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.florian.lydia.technicaltest.R
+
+class NavHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_host)
+    }
+}
--- app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
+++ app/src/main/java/fr/florian/lydia/technicaltest/ui/views/SplashActivity.kt
@@ -1,21 +1,19 @@
 package fr.florian.lydia.technicaltest.ui.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import fr.florian.lydia.technicaltest.R
-import fr.florian.lydia.technicaltest.data.models.User
-import fr.florian.lydia.technicaltest.ui.viewmodels.SplashViewModel
-import java.io.Console
+import fr.florian.lydia.technicaltest.ui.viewmodels.UserListViewModel
 
 class SplashActivity : AppCompatActivity() {
 
-    lateinit var userViewModel: SplashViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+//        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(this, NavHostActivity::class.java))
+        finish()
     }
 }
